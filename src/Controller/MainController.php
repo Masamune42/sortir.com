3bb0ea5ed3a5cc9c63067b8b6e9faabb83ee72b0
Alexis <claich.alexis@gmail.com>
2020-02-10 16:18:09 +0100

    commit after change in MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
@@ -13,5 +14,20 @@ class MainController extends AbstractController
     public function index()
     {
         return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        //get the login error if there is one
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        //last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('main/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,]);
     }
 }
