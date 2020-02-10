8197f41ed3e9701ed7909b65a871b830ab640826
vianney <60422994+SlothVia@users.noreply.github.com>
2020-02-10 17:25:06 +0100

    commit after add form user and profil.twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\UserType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,7 +20,7 @@ class MainController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function login(AuthenticationUtils $authenticationUtils)
+    public function login(AuthenticationUtils $authenticationUtils, Request $request)
     {
         //get the login error if there is one
 
@@ -32,28 +29,8 @@ class MainController extends AbstractController
         //last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+
+
         return $this->render('main/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,]);
-    }
-
-    /**
-     * @Route("/profil", name ="profil")
-     */
-    public function profil(EntityManagerInterface $entityManager, Request $request)
-    {
-        $user = new User();
-        $profilForm = $this->createForm(UserType::class, $user);
-        $profilForm->handleRequest($request);
-
-        if ($profilForm->isSubmitted() && $profilForm->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            $this->redirectToRoute('main/home.html.twig', ['id' => $user->getId()]);
-        }
-
-        $profilFormView = $profilForm->createView();
-
-        return $this->render('main/profil.html.twig', compact('profilFormView'));
-
     }
 }
