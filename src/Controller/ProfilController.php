7a92babf815166cebad4deea7c821d9f5ef8bb50
vianney <60422994+SlothVia@users.noreply.github.com>
2020-02-11 16:12:38 +0100

    commit after add in function myprofil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\MyUserType;
+use App\Form\PasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,8 +18,21 @@ class ProfilController extends AbstractController
     public function myprofil(EntityManagerInterface $entityManager, Request $request)
     {
         $user = new User();
+        $password = new User();
+        $user->setUsername($this->getUser()->getUsername());
+        $user->setName($this->getUser()->getName());
+        $user->setFirstname($this->getUser()->getFirstname());
+        $user->setPhone($this->getUser()->getPhone());
+        $user->setMail($this->getUser()->getMail());
+        $user->setEstablishment($this->getUser()->getEstablishment());
+
+
         $profilForm = $this->createForm(MyUserType::class, $user);
         $profilForm->handleRequest($request);
+
+        $passwordForm = $this->createForm(PasswordType::class);
+        $passwordForm->handleRequest($request);
+
 
         if ($profilForm->isSubmitted() && $profilForm->isValid()) {
             $entityManager->persist($user);
@@ -27,21 +41,35 @@ class ProfilController extends AbstractController
             $this->redirectToRoute('main/home.html.twig', ['id' => $user->getId()]);
         }
 
+
+        if ($passwordForm->isSubmitted() && $passwordForm->isValid()) {
+            $entityManager->persist($password);
+            $entityManager->flush();
+
+            $this->redirectToRoute('main/home.html.twig', ['id' => $password->getId()]);
+        }
+
         $profilFormView = $profilForm->createView();
+        $passwordFormView = $passwordForm->createView();
 
-        return $this->render('main/myprofil.html.twig', compact('profilFormView'));
-
+        return $this->render(
+            'main/myprofil.html.twig',
+            compact('profilFormView', $user, 'passwordFormView', $password)
+        );
     }
 
-    /**
-     * @Route("/profils/{id}", name="profils")
-     */
-    public function profils($id, entityManagerInterface $entityManager)
-    {
-        $userRepository = $entityManager->getRepository(User::class);
+        /**
+         * @Route("/profils/{id}", name="profils")
+         */
+        public
+        function profils($id, entityManagerInterface $entityManager)
+        {
+            $userRepository = $entityManager->getRepository(User::class);
 
-        $user = $userRepository->find($id);
+            $user = $userRepository->find($id);
 
-        return $this->render('profil/profils.html.twig', compact('user'));
-    }
+            return $this->render('profil/profils.html.twig', compact('user'));
+        }
+
+
 }
