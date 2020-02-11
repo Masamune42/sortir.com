e6308ebf680a7adcd6f8653a6d64c759dada7830
Alexis <claich.alexis@gmail.com>
2020-02-11 08:28:16 +0100

    commit Tuesday the begginning

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,9 @@ use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -15,8 +17,21 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            $response = new Response(
+                '',
+                Response::HTTP_OK,
+                ['content-type' => 'text/html']
+            );
+
+            $response->headers->setCookie(Cookie::create('lastUsername', $this->getUser()->getUsername()));
+
+            $response->prepare($request);
+            $response->send();
+        }
+
         return $this->render('main/index.html.twig');
     }
 
@@ -26,13 +41,12 @@ class MainController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils, Request $request)
     {
         //get the login error if there is one
-
         $error = $authenticationUtils->getLastAuthenticationError();
 
         //last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-
+        //verify the existance of a username already used
 
         return $this->render('main/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,]);
     }
