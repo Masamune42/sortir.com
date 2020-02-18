246d2d82e356a98f683e784b153d9c9b21fe7379
alex <a.caignard@hotmail.fr>
2020-02-18 15:26:45 +0100

    auto delete old picture when upload another one. button to simply delete the profil picture added

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -7,9 +7,12 @@ use App\Entity\User;
 use App\Form\CSVType;
 use App\Form\MyUserType;
 use App\Form\PasswordType;
+use App\Kernel;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use League\Csv\Reader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,17 +33,17 @@ class ProfilController extends AbstractController
     public function myprofil(
         EntityManagerInterface $entityManager,
         EncoderFactoryInterface $encoderFactory,
-        Request $request, UploadProfilPic $uploadProfilPic
-    )
-    {
+        Request $request,
+        UploadProfilPic $uploadProfilPic
+    ) {
 //        phpinfo();
         $user = $this->getUser();
 
         $encoder = $encoderFactory->getEncoder($user);
 
 
-        $newp=$request->request->get('my_user')['newpassword']['first'];
-        
+        $newp = $request->request->get('my_user')['newpassword']['first'];
+
         // dump($request);
 
 
@@ -52,20 +55,32 @@ class ProfilController extends AbstractController
             $user->getSalt()
         );
 
-
         $profilForm = $this->createForm(MyUserType::class, $user);
+
+        $oldPicturePath = null;
+
+        if ($user->getPicture()) {
+            $pathdir = \dirname(__DIR__);
+            $oldPicturePath = $pathdir.'/../public/images/profil/'.$user->getPicture();
+            //die($oldPicturePath);
+        }
+
         $profilForm->handleRequest($request);
 
-
         if ($profilForm->isSubmitted() && $profilForm->isValid() && $validPassword) {
-            if (!empty($newp)){
+            if (!empty($newp)) {
                 $user->setPassword($encoder->encodePassword($newp, $user->getSalt()));
             }
+
             $picture = $profilForm->get('picture')->getData();
 
             if ($picture) {
                 $pictureName = $uploadProfilPic->upload($picture);
                 $user->setPicture($pictureName);
+
+                if ($oldPicturePath) {
+                    unlink($oldPicturePath);
+                }
             }
 
             $this->addFlash('success', 'Profil modifié !');
@@ -75,13 +90,13 @@ class ProfilController extends AbstractController
 
             $this->redirectToRoute('profil_myprofil');
 
-        } else if ($profilForm->isSubmitted() && !$validPassword) {
-            $this->addFlash('error', 'Mot de passe erroné !');
+        } else {
+            if ($profilForm->isSubmitted() && !$validPassword) {
+                $this->addFlash('error', 'Mot de passe erroné !');
+            }
         }
 
-
         $profilFormView = $profilForm->createView();
-
 
         return $this->render(
             'main/myprofil.html.twig',
@@ -95,6 +110,23 @@ class ProfilController extends AbstractController
     public function profil(User $user)
     {
         return $this->render('profil/profils.html.twig', compact('user'));
+    }
+
+    /**
+     * @Route("/deletepicture/{id}", name="delete_pic", requirements={"id" : "\d+"})
+     */
+    public function delete(User $user, EntityManagerInterface $entityManager)
+    {
+        $pathdir = \dirname(__DIR__);
+        $oldPicturePath = $pathdir.'/../public/images/profil/'.$user->getPicture();
+        unlink($oldPicturePath);
+
+        $user->setPicture(null);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("profil_myprofil");
+
     }
 
 }
