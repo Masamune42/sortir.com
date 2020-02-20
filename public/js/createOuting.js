65ffc88484eaf9667a2e6ca168feee4910910461
Alexis <claich.alexis@gmail.com>
2020-02-20 16:16:51 +0100

    commit to update the firefox error create

--- public/js/createOuting.js
+++ public/js/createOuting.js
@@ -8,11 +8,16 @@ if (isFirefox) {
     dateTimeStart.innerHTML = `<label for="date_start_firefox">Date de début de sortie</label>
 <span class="d-flex flex-wrap">
  <input type="date" id="date_start_firefox" name="date_start_firefox" class="input-group-text" style="margin-right: auto;margin-left: auto"/><input type="time" id="time_start_firefox" name="time_start_firefox" class="input-group-text" style="margin-right: auto;margin-left: auto"/>
-</span>`;
+</span>
+  {{ form_row(outingFormView.startTime) }}`;
     dateTimeLimit.innerHTML = `<label for="date_limit_firefox">Date limite d'inscription</label>
 <span class="d-flex flex-wrap">
  <input type="date" id="date_limit_firefox" name="date_limit_firefox" class="input-group-text" style="margin-right: auto;margin-left: auto"/><input type="time" id="time_limit_firefox" name="time_limit_firefox" class="input-group-text" style="margin-right: auto;margin-left: auto"/>
-</span>`;
+</span>
+<div style="display: none">
+{{ form_label(outingFormView.limitDateTime) }}
+            {{ form_widget(outingFormView.limitDateTime) }}</div>
+            {{ form_errors(outingFormView.limitDateTime) }}`;
 }
 
 
